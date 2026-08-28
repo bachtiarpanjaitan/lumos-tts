33380fb494c46189d7c7577cad3b152ae5bd2902
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2026-08-29 04:07:49 +0700

    feat: migrate TTS backend from Piper to Groq API and update environment configuration

--- app.py
+++ app.py
@@ -1,67 +1,43 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from groq import Groq
+from fastapi.responses import StreamingResponse
 from fastapi import FastAPI
 from pydantic import BaseModel
-from fastapi.responses import FileResponse
-from piper.voice import PiperVoice
-import wave
-import uuid
-import os
 import uvicorn
-from io import BytesIO
-from fastapi.responses import StreamingResponse
-from dotenv import load_dotenv
+
 load_dotenv()
-
 app = FastAPI()
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-MODEL_PATH =  os.getenv(
-   "MODEL_PATH",
-   "models/id_ID-news_tts-medium.onnx"
-)
-voice = PiperVoice.load(MODEL_PATH)
+
 class TTSRequest(BaseModel):
     text: str
+    model: str = "canopylabs/orpheus-v1-english"
+    voice: str = "autumn"
 
-# @app.post("/tts")
-# async def tts(request: TTSRequest):
-
-#     os.makedirs("output", exist_ok=True)
-
-#     filename = f"output/{uuid.uuid4()}.wav"
-
-#     with wave.open(filename, "wb") as wav_file:
-#         voice.synthesize_wav(
-#             request.text,
-#             wav_file
-#         )
-
-#     return FileResponse(
-#         path=filename,
-#         media_type="audio/wav",
-#         filename="speech.wav"
-#     )
 
 @app.post("/tts")
-async def tts(request: TTSRequest):
-
-    buffer = BytesIO()
-
-    with wave.open(buffer, "wb") as wav_file:
-        voice.synthesize_wav(
-            request.text,
-            wav_file
-        )
-
-    buffer.seek(0)
-
-    return StreamingResponse(
-        buffer,
-        media_type="audio/wav"
+async def tts_stream(req: TTSRequest):
+    response = client.audio.speech.create(
+        model=req.model,
+        voice=req.voice,
+        response_format="wav",
+        input=req.text,
     )
+
+    def gen():
+        for chunk in response.iter_bytes(8192):
+            yield chunk
+
+    return StreamingResponse(gen(), media_type="audio/wav")
+
 
 if __name__ == "__main__":
     uvicorn.run(
         "app:app",
-        host=os.getenv("HOST", "0.0.0.0"),
-        port=int(os.getenv("PORT", "8000")),
-        reload=True
+        host=os.getenv("HOST", "127.0.0.1"),
+        port=int(os.getenv("PORT", "8001")),
+        reload=True,
     )
