b2aada4057d98eaa31a536b35c17a97c772253e2
Bachtiar Panjaitan <bachtiarpanjaitan0@gmail.com>
2026-09-13 20:15:43 +0700

    fix: auto reload uvicorn

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from pydantic import functional_serializers
 import os
 from pathlib import Path
 from dotenv import load_dotenv
@@ -47,5 +48,5 @@ if __name__ == "__main__":
         "app:app",
         host=os.getenv("HOST", "127.0.0.1"),
         port=int(os.getenv("PORT", "8001")),
-        reload=True,
+        reload=False,
     )
